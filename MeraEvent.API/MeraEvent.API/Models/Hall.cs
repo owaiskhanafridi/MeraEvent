bb930f2b01d6e09af6d61cda25b56214c3813af8
Owais Khan <MY_NAME@example.com>
2023-07-29 23:00:17 -0400

    Added DB connection and models.

---
+++ MeraEvent.API/MeraEvent.API/Models/Hall.cs
@@ -0,0 +1,11 @@
+﻿namespace MeraEvent.API.Models
+{
+    public class Hall
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;   
+        public string Description { get; set; } = String.Empty;
+        public int Rent { get; set; }
+
+    }
+}
---
+++ MeraEvent.API/MeraEvent.API/Models/Location.cs
@@ -0,0 +1,10 @@
+﻿    namespace MeraEvent.API.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string Area { get; set; } = string.Empty;
+        public string Landmark { get; set; } = string.Empty;
+    }
+}
